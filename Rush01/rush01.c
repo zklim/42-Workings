3bd58705ef6a3707fd2569068d87e6c155a27c28
Zhee Khang Lim <zhlim@u91z01s03.42kl.edu.my>
2022-08-28 20:01:27 +0800

    Rush01

--- Rush01/rush01.c
+++ Rush01/rush01.c
@@ -143,6 +143,10 @@ void	ft_ifcol14(char *inputs, char *outputs)
 				{
 					ft_answerscol(i, inputs, outputs);
 				}
+				if (inputs[i + 4] != '4')
+				{
+					outputs[i] = '4';
+				}
 			}
 			if (inputs[i] == '4') 
 			{
@@ -158,15 +162,21 @@ void	ft_ifcol14(char *inputs, char *outputs)
 void	ft_ifrow14(char *inputs, char *outputs)
 {
 	int		i;
+	int		j;
 
 	i = 8;
 	while (i < 12)
 		{
+			j = (i - 8) * 4;
 			if (inputs[i] == '1') 
 			{
 				if (inputs[i + 4] == '4')
 				{
 					ft_answersrow(i, inputs, outputs);
+				}
+				if (inputs[i + 4] != '4')
+				{
+					outputs[j] = '4';
 				}
 			}
 			if (inputs[i] == '4') 
@@ -208,6 +218,146 @@ void	ft_putchar(char *outputs)
 	}
 }
 
+void	fill_lastdig(char *repeatedcol, char *repeatedrow, char *outputs, int j)
+{
+	int i;
+	int	count;
+	int	count2;
+	int	col;
+	int	row;
+
+	i = 0;
+	count = 0;
+	count2 = 0;
+	while (i < 4)
+	{
+		if (repeatedcol[i] == 'e')
+		{
+			count++;
+		}
+		if (repeatedrow[i] == 'e')
+		{
+			count2++;
+		}
+		i++;
+	}
+	if (count == 1 && count2 == 1)
+	{
+		row = 0;
+		col = 0;
+		while (col < 4)
+		{
+			if (repeatedcol[col] == 'e')
+			{
+				while (row < 4)
+				{
+					if(repeatedrow[row] == 'e')
+					{
+						outputs[col + row * 4] = '1' + j;
+					}
+					row++;
+				}
+			}
+			col++;
+		}
+	}
+}
+
+void	ft_lastdigit(char *outputs)
+{
+	int j;
+	int	col;
+	int	row;
+	char *repeatedcol;
+	char *repeatedrow;
+
+	j = 0;
+	repeatedcol = malloc(4);
+	repeatedrow = malloc(4);
+	while (j < 4)
+	{
+		row = 0;
+		while (row < 4)
+		{
+			col = 0;
+			while (col < 4)
+			{
+				if (outputs[row * 4 + col] == '1' + j)
+				{
+					repeatedcol[row] = '0' + col;
+					col = 0;
+				break ;
+				}
+				if (col == 3)
+				{
+					repeatedcol[row] = 'e';
+				}
+				col++;
+			}
+			row++;
+		}
+		col = 0;
+		while (col < 4)
+		{
+			row = 0;
+			while (row < 4)
+			{
+				if (outputs[col + row * 4] == '1' + j)
+				{
+					repeatedrow[col] = '0' + row;
+					row = 0;
+					break ;
+				}
+				if (row == 3)
+				{
+					repeatedrow[col] = 'e';
+				}
+				row++;
+			}
+			col++;
+		}
+		fill_lastdig(repeatedcol, repeatedrow, outputs, j);
+		j++;
+	}
+	free(repeatedcol);
+	free(repeatedrow);
+}
+
+void	ft_scanempty(char *inputs, char *outputs)
+{
+	int	a;
+	int i;
+	int	j;
+	int	col;
+	int	row;
+
+	a = 4;
+	i = 0;
+	j = 0;
+	while (i < a)
+	{
+		while (j < a)
+		{
+			if (!(outputs[i * a + j] >= '0' && outputs[i * a + j] <= '9'))
+			{
+				ft_lastdigit(outputs);
+				col = (i * a + j) % 4;
+				row = (i * a + j) / 4;
+				if (inputs[col] == '3' && inputs[col + 4] == '2')
+				{
+					if (inputs[row + 8] == '3' && inputs[row + 12] == '2')
+					{
+						outputs[i * a + j] = '3';
+					}
+				}
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+}
+
 int	main(int ac, char **av)
 {
 	char	*inputs;
@@ -221,6 +371,7 @@ int	main(int ac, char **av)
 			inputs = ft_triminputs(av[1]);
 			ft_ifcol14(inputs, outputs);
 			ft_ifrow14(inputs, outputs);
+			ft_scanempty(inputs, outputs);
 			ft_putchar(outputs);
 			free(inputs);
 			free(outputs);
